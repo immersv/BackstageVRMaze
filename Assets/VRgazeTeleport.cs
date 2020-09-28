8e29d2fda9edc76b93d19122d15b2abb2ae6fb63
immersvin <immersv@gmail.com>
2020-09-28 11:21:50 +0530

    Gaze Timer Point updated

---
+++ Assets/VRgazeTeleport.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRgazeTeleport : MonoBehaviour
+{
+    public Image gazeImage;
+    public float gazeTimer;
+    public bool gazeStatus;
+    public float gazeTotaltime=2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gazeStatus)
+        {
+            gazeTimer += Time.deltaTime;
+            gazeImage.fillAmount = gazeTimer / gazeTotaltime;
+        }
+        else
+        {
+
+        }
+    }
+
+   public void OnGazeEnter()
+    {
+        gazeStatus = true;
+    }
+    public void OnGazeExit()
+    {
+        gazeStatus = false;
+        gazeTimer = 0;
+        gazeImage.fillAmount = gazeTimer / gazeTotaltime;
+    }
+}
