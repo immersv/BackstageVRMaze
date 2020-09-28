a73d871b531b676597ea65a130f2d34599b78388
immersvin <immersv@gmail.com>
2020-09-28 10:35:09 +0530

    Backstage VR Maze Game Created

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BackstageVRApp
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        private CharacterController characterController;
+        [SerializeField]
+        private float moveSpeed=1.5f;
+        private bool iswalking = false;
+        [SerializeField]
+        private bool comfortMode = false;
+        // Start is called before the first frame update
+        [SerializeField]
+        private bool isRotating = false;
+        [SerializeField]
+        private float camRotateDegrees = 45f;
+        void Awake()
+        {
+            characterController = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //PlayerMoveForward();
+            if (Input.GetMouseButtonUp(0))
+                {
+                iswalking = !iswalking;
+                }
+            if (iswalking)
+            {
+                PlayerMoveForward();
+                isRotating = true;
+            }
+               
+            if(comfortMode && isRotating && !iswalking)
+            {
+                OnCamRotateEnable();
+            }
+
+        }
+        void PlayerMoveForward()
+        {
+            Vector3 playerComfortDirection = transform.forward * moveSpeed;
+            Vector3 playerMoveDirection = Camera.main.transform.forward * moveSpeed;
+            if (comfortMode == true)
+            {
+                characterController.SimpleMove(playerComfortDirection);
+            }
+            else
+            {
+                characterController.SimpleMove(playerMoveDirection); ;
+            }
+           
+        }
+        void OnCamRotateEnable()
+        {
+            float horizontalAxis = Input.GetAxis("Horizontal");
+            if (horizontalAxis > 0)
+            {
+                CamSnapRotation(camRotateDegrees);
+            }
+            else if (horizontalAxis < 0)
+            {
+                CamSnapRotation(-camRotateDegrees);
+            }
+            else if(horizontalAxis==0)
+            {
+                isRotating = true;
+            }
+        }
+
+        void CamSnapRotation(float degrees)
+        {
+            transform.Rotate(0, degrees, 0);
+            isRotating = false;
+        }
+
+    }
+}
+
---
+++ Assets/VRInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRInteraction : MonoBehaviour
+{
+    public GameObject cube;
+    public void OnPointerClick()
+    {      
+        //Get the Renderer component from the new cube
+        var cubeRenderer = cube.GetComponent<Renderer>();
+
+        //Call SetColor using the shader property name "_Color" and setting the color to red
+        cubeRenderer.material.color = Color.black;
+    }
+    public void OnPointerEnter()
+    {
+        var cubeRenderer = cube.GetComponent<Renderer>();
+
+        //Call SetColor using the shader property name "_Color" and setting the color to red
+        cubeRenderer.material.color = Color.blue;
+    }
+    public void OnPointerExit()
+    {
+        var cubeRenderer = cube.GetComponent<Renderer>();
+
+        //Call SetColor using the shader property name "_Color" and setting the color to red
+        cubeRenderer.material.color = Color.green;
+    }
+}
