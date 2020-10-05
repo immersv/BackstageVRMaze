86e0ded7aa9982db9b7178aff7c4b0ef15e24558
immersvin <immersv@gmail.com>
2020-10-05 12:44:39 +0530

    no message

---
+++ Assets/UIManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerController : MonoBehaviour
+{
+    public GameObject mainCanvas, rulesCanvas;
+    // Start is called before the first frame update
+    
+    public void ToggleCanvas()
+    {
+        mainCanvas.SetActive(false);
+        rulesCanvas.SetActive(true);
+    }
+    public void GoToPlayScene(int value)
+    {
+        SceneManager.LoadScene(value);
+    }
+}
