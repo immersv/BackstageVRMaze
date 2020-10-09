ef75d9954578f504e363238d19986bbead66e5a6
immersvin <immersv@gmail.com>
2020-10-09 12:18:40 +0530

    no message

---
+++ Assets/EthenPLayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthenPLayerMovement : MonoBehaviour
+{
+    //this script will make you to move in navmesh spaceor movable space
+    public GameObject groundPlane;
+    private Transform mainCam;
+    Ray ray;
+    RaycastHit raycastHit;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mainCam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(mainCam.position, mainCam.forward*50,Color.red);
+        if (Input.GetMouseButtonUp(0))
+        {
+            OnEthenRaycast();
+        }
+    }
+
+    private void OnEthenRaycast()
+    {
+        ray = new Ray(mainCam.position, mainCam.forward);
+        if(Physics.Raycast(ray, out raycastHit, Mathf.Infinity))
+        {
+           GameObject obj= raycastHit.collider.gameObject;
+            if(obj== groundPlane)
+            {
+                transform.position = raycastHit.point;
+            }
+        }
+    }
+}
---
+++ Assets/PlayerTargetMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTargetMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject groundPlane;
+    private Transform mainCam;
+    private Ray ray;
+    private RaycastHit raycastHit;
+    void Awake()
+    {
+        mainCam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(mainCam.position, mainCam.forward * 75, Color.red);
+        if (Input.GetMouseButtonUp(0))
+        {
+            OnClickRaycast();
+        }
+         
+    }
+
+    private void OnClickRaycast()
+    {
+        ray = new Ray(mainCam.position,mainCam.forward);
+        if(Physics.Raycast(ray, out raycastHit, Mathf.Infinity))
+        {
+            GameObject obj = raycastHit.collider.gameObject;
+            if (obj == groundPlane)
+            {
+                transform.position = raycastHit.point;
+            }
+        }
+    }
+}
